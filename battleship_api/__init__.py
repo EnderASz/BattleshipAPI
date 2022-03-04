cff3029234900c3eb511c7422cfd3544b6f6c1be
EnderASz <olek5188@gmail.com>
2022-03-04 10:44:10 +0100

    App factory: Add docstring

--- battleship_api/__init__.py
+++ battleship_api/__init__.py
@@ -5,6 +5,20 @@ from .core.settings import Settings, get_settings
 
 
 def create_app(settings: Settings | None = None) -> FastAPI:
+    """
+    Function initializing application instance with given settings.
+    If settings not provided, function takes care of creating them,
+    from default ones, environment variables and if is it possible config file.
+
+    Params:
+        - [Optional] settings - Settings object instance.
+            - If not provided, default settings are used and overwrited by
+              environment variables and configuration file settings if
+              available.
+
+    Returns:
+        FastAPI: BattleshipAPI app instance
+    """
     if settings is None:
         settings = get_settings()
     if settings.debug:
