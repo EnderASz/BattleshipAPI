25cd813f377735e562c3f31cd336e9da9568ec9b
EnderASz <olek5188@gmail.com>
2022-03-13 21:35:06 +0100

    Board: Add invalid and missing access password exceptions

--- battleship_api/api/board/exceptions.py
+++ battleship_api/api/board/exceptions.py
@@ -14,3 +14,19 @@ class BoardNotFoundException(BaseAPIException):
     code = status.HTTP_404_NOT_FOUND
     message = "Board not found"
     schema = schemas.BoardSearch
+
+
+class MissingBoardPasswordException(BaseAPIException):
+    """
+    API exception raise when missing board access password.
+    """
+    code = status.HTTP_401_UNAUTHORIZED
+    message = "Board access requires password"
+
+
+class InvalidBoardPasswordException(BaseAPIException):
+    """
+    API exception raise when invalid board access password is provided.
+    """
+    code = status.HTTP_403_FORBIDDEN
+    message = "Board access password is incorrect"
