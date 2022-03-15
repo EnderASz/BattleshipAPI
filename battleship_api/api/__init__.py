1941b2eb86a09a14e49bbfb999da5776322e2d37
EnderASz <olek5188@gmail.com>
2022-03-15 08:09:17 +0100

    API: Init ship package for ships related operations and data management

--- battleship_api/api/__init__.py
+++ battleship_api/api/__init__.py
@@ -2,17 +2,21 @@ from fastapi import APIRouter
 
 from .board.routes import router as board_router
 from .player.routes import router as player_router
+from .ship.routes import router as ship_router
 
 from .board.tags import all_tags as board_tags
 from .player.tags import all_tags as player_tags
+from .ship.tags import all_tags as ship_tags
 
 
 api_tags = (
     board_tags
-    + player_tags)
+    + player_tags
+    + ship_tags)
 
 
 api_router = APIRouter(prefix='/api')
 
 api_router.include_router(board_router)
 api_router.include_router(player_router)
+api_router.include_router(ship_router)
---
+++ battleship_api/api/ship/tags.py
@@ -0,0 +1,10 @@
+ships_operation = {
+    'name': 'ships-operation',
+    'description':
+        "Operations with ships.<br>"
+        "Each ship must be assigned to it's owner (player)."}
+
+
+all_tags = [
+    ships_operation,
+]
