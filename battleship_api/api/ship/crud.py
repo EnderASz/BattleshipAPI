b9c908eb67e5bc703b58e61623a0b3cfb9bc6771
EnderASz <olek5188@gmail.com>
2022-03-16 15:38:51 +0100

    Ship: Add crud tools for getting and creating objects

---
+++ battleship_api/api/ship/crud.py
@@ -0,0 +1,58 @@
+from sqlalchemy.sql import Session
+
+from .schemas import ShipCreate as ShipCreateSchema
+from .models import Ship as ShipModel
+from battleship_api.api.player.models import Player as PlayerModel
+
+
+def create_ship(
+    db: Session,
+    ship: ShipCreateSchema,
+    owner: PlayerModel
+) -> ShipModel:
+    """
+    Creates ship instance assigned to player given via `owner` and adds it to
+    the database.
+
+    Params:
+        - db: Database session
+        - ship: Ship creation data represented by
+            `battleship_api.api.ship.schemas.ShipCreate` schema
+        - owner: Player database model instance to which ship will be assigned
+
+    Returns:
+        New ship database object instance.
+    """
+    owner.ships.append(ship := ShipModel(**ship.dict()))
+    db.commit()
+    db.refresh(ship)
+    return ship
+
+
+def get_ship(db: Session, ship_id: int) -> ShipModel | None:
+    """
+    Returns ship object from database searched by ship id.
+
+    Params:
+        - db (Session): Database session
+        - ship_id: Ship id
+
+    Returns:
+        Ship database object instance
+    """
+    return db.query(ShipModel).filter(ShipModel.id == ship_id).first()
+
+
+def get_ships(db: Session, limit: int, offset: int) -> list[ShipModel]:
+    """
+    Returns list of `limit` ships in database starting from `offset` ship.
+
+    Params:
+        - db: Database session
+        - limit: Number of ships to return
+        - offset: Number of ships to skip
+
+    Returns:
+        Ship list of `limit` elements starting from `offset` ship.
+    """
+    return db.query(ShipModel).offset(offset).limit(limit).all()
