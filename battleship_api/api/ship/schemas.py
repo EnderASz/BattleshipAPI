2377d1ff292bdf5ee9ede566a68ecf58aae95384
EnderASz <olek5188@gmail.com>
2022-03-20 13:53:22 +0100

    Ship: Create schema for location data only - Replace rotation with orientation - Add rule to ship length that must be between 1 and 4

--- battleship_api/api/ship/schemas.py
+++ battleship_api/api/ship/schemas.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel as BaseSchema, Field, validator
+from pydantic import BaseModel as BaseSchema, Field
 
-from .utils import Rotation
+from .utils import Orientation
 
 
-class ShipRestricted(BaseSchema):
-    length: int
+class ShipLocation(BaseSchema):
+    length: int = Field(..., ge=1, le=4)
     column: int = Field(..., ge=1, le=10)
     row: int = Field(..., ge=1, le=10)
-    rotation: Rotation
+    orientation: Orientation
+
+
+class ShipRestricted(ShipLocation):
+    pass
 
 
 class ShipCreate(ShipRestricted):
