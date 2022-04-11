cd272e870c1112ae37f949ad310df834b002c784
EnderASz <olek5188@gmail.com>
2022-04-11 22:47:25 +0200

    Shot: Add shot-related operations documentation tag

---
+++ battleship_api/api/shot/tags.py
@@ -0,0 +1,10 @@
+shots_operation = {
+    'name': 'shots-operation',
+    'description':
+        "Operations with shots.<br>"
+        "Each shot must be assigned to shooter (player)."}
+
+
+all_tags = [
+    shots_operation,
+]
